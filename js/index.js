45646029a90cbda5c67788ab5e5d50f93567fc6f
basitol <41623944+basitol@users.noreply.github.com>
2021-12-13 22:43:54 +0100

    writing media query for desktop screens and javascript for changing number of orders

--- js/index.js
+++ js/index.js
@@ -6,8 +6,18 @@ const btnMenuClose = document.querySelector("#btnMenuClose");
 const cart = document.querySelector(".cart");
 const btnCart = document.querySelector(".btn-cart");
 
+const btnPlus = document.querySelector("#btnPlus");
+const btnMinus = document.querySelector("#btnMinus");
+const productCounter = document.querySelector(".counter");
+
+// Numerical value
+let productCounterValue = 1;
+
 btnHamburger.addEventListener("click", onHamburgerClick);
 btnMenuClose.addEventListener("click", onMenuClose);
+
+btnPlus.addEventListener("click", productPlus);
+btnMinus.addEventListener("click", productMinus);
 
 function onHamburgerClick() {
   menu.classList.remove("hidden");
@@ -22,22 +32,20 @@ btnCart.addEventListener("click", openCart);
 function openCart() {
   cart.classList.toggle("hidden");
 }
-// btnHamburger.addEventListener("click", function () {
-//   console.log("click hamburger");
 
-//   if (header.classList.contains("open")) {
-//     console.log("open");
-//     header.classList.remove("open");
-//     slideElems.forEach(function (element) {
-//       element.classList.toggle("slide-in");
-//       element.classList.add("slide-out");
-//     });
-//   } else {
-//     console.log("close");
-//     header.classList.add("open");
-//     slideElems.forEach(function (element) {
-//       element.classList.remove("slide-out");
-//       element.classList.add("slide-in");
-//     });
-//   }
-// });
+function productPlus() {
+  // console.log(productCounterValue);
+  setProductCounter(1);
+}
+
+function productMinus() {
+  setProductCounter(-1);
+}
+
+function setProductCounter(value) {
+  if (productCounterValue + value > 0) {
+    productCounterValue += value;
+    productCounter.innerHTML = productCounterValue;
+  }
+  // console.log(value);
+}
